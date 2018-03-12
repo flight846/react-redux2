fa39d675ee57a6367c595ca25417ae70a3fcbb86
yazid <yazid@decision-science.com>
2018-03-13 02:07:34 +0800

    Init redux containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'Eloquent Javascript' },
+        { title: 'Harry Porter' },
+        { title: 'The Dark Tower' },
+    ]
+}
