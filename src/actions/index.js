57565001cddc53bdc86a72d0b6e086e21112482a
Yazid <yazid@decision-science.com>
2018-03-13 18:23:10 +0800

    Add selected book action

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+// action creator
+export function selectBook(book) {
+    // selectBook is an actionCreator, it needs to return an action,
+    // an object with a type property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
+}
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 // make this into a react-redux containers
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={ book.title } className="list-group-item">
+                <li
+                    key={ book.title }
+                    onClick={ () => this.props.selectBook(book) }
+                    className="list-group-item">
                     { book.title }
                 </li>
             )
@@ -32,5 +37,16 @@ function mapStateToProps(state) {
     }
 }
 
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+    // whenever selectBook is called, the result should be passed
+    // to all of our reducer
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
 // export only containers => react redux components with states
-export default connect(mapStateToProps)(BookList);
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
